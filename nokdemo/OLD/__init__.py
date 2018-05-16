696a5a156b4efe316d138416f24ad07c4ef1139c
luis <luis@centos7.localdomain>
2018-05-16 16:06:26 +0100

    deleting stuff

--- nokdemo/OLD/__init__.py
+++
@@ -1,1 +0,0 @@
-from .nokdemo import app
--- nokdemo/OLD/forms.py
+++
@@ -1,5 +0,0 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-
-class MSSCommandsForm(Form):
-    net = StringField('NET', [validators.Length(min=4, max=25)])
-    spc = StringField('SPC', [validators.Length(min=6, max=35)])
